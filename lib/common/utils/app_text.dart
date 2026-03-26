31d702a63a5524ce4d3aa7f526671805724be413
mohamedgameel3201 <106116066+mohamedgameel3201@users.noreply.github.com>
2026-03-26 23:11:45 +0200

    Update app_text.dart

--- lib/common/utils/app_text.dart
+++ lib/common/utils/app_text.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:webinar/config/l10n/app_localizations.dart';
 import 'package:webinar/common/data/app_language.dart';
 import 'package:webinar/locator.dart';
 
